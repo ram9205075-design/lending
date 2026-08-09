ca3bc7065dd2c7bb816ff39850a9b309c347e484
ram9205075-design <ram9205075@gmail.com>
2026-08-09 11:31:13 +0530

    Refactor WhatsApp settings in script.js

--- script.js
+++ script.js
@@ -1,43 +1,26 @@
- // =====================================
+// =====================================
 // WHATSAPP SETTINGS
 // =====================================
 
-// Apna WhatsApp number yahan daalo
-// Example: India +63 9752666808
-const whatsappNumber = "+63 9752666808";
+const whatsappNumber = "639752666808";
 
-
-// WhatsApp par pre-filled message
 const message =
     "Hello, I want to join the WhatsApp group.";
 
-
-//
-// WhatsApp URL
-//
 const whatsappLink =
     "https://wa.me/" +
     whatsappNumber +
     "?text=" +
     encodeURIComponent(message);
 
-
-// Button select karo
 const whatsappBtn =
     document.getElementById("whatsappBtn");
 
-
-// Link set karo
 whatsappBtn.href = whatsappLink;
 
-
-// Click handling
 whatsappBtn.addEventListener(
     "click",
     function () {
-
-        window.location.href =
-            whatsappLink;
-
+        window.location.href = whatsappLink;
     }
 );
