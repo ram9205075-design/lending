9b86e0935f15b3e1abb90a1334bb6dcf3bd06f31
ram9205075-design <ram9205075@gmail.com>
2026-08-09 11:47:49 +0530

    Refactor WhatsApp button setup and link creation

--- script.js
+++ script.js
@@ -7,20 +7,39 @@ const whatsappNumber = "639752666808";
 const message =
     "Hello, I want to join the WhatsApp group.";
 
+
+// =====================================
+// CREATE WHATSAPP LINK
+// =====================================
+
 const whatsappLink =
     "https://wa.me/" +
     whatsappNumber +
     "?text=" +
     encodeURIComponent(message);
 
-const whatsappBtn =
-    document.getElementById("whatsappBtn");
 
+// =====================================
+// BUTTON
+// =====================================
+
+const whatsappBtn =
+    document.getElementById(
+        "floatingWhatsappBtn"
+    );
+
+
+// Set WhatsApp URL
 whatsappBtn.href = whatsappLink;
 
+
+// Open WhatsApp
 whatsappBtn.addEventListener(
     "click",
     function () {
-        window.location.href = whatsappLink;
+
+        window.location.href =
+            whatsappLink;
+
     }
 );
